e39db1e1f70399d25068a30aae29b28715d06cb9
IRahman2024 <stormfuryultra784@gmail.com>
2024-03-20 03:17:58 +0600

    did some job on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Link from '../link/Link';
+// import Link from '../link/Link'
+import { AiOutlineMenuUnfold } from "react-icons/ai";
+import { RxCross1 } from "react-icons/rx";
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(true);
+
     const routes = [
         { id: 1, path: '/', name: 'Home' },
         { id: 2, path: '/about', name: 'About' },
         { id: 3, path: '/services', name: 'Services' },
         { id: 4, path: '/contact', name: 'Contact' },
         { id: 5, path: '/products', name: 'Products' }
-      ];      
-    
+    ];
+
     return (
         <div>
-            <ul className='flex'>
+            <div className='md:hidden text-2xl' onClick={() => setOpen(!open)}>
                 {
-                    routes.map((route, idx) => <li className='text-3xl' key={idx}><a className='text-3xl' href={route.path}>{route.name}</a></li>)
-                }    
-            </ul>    
+                    // open === true ? 'open' : 'close';
+
+                    open === true ? <AiOutlineMenuUnfold></AiOutlineMenuUnfold> : <RxCross1></RxCross1>
+                }
+                {/* <button><AiOutlineMenuUnfold className='' /></button> */}
+            </div>
+            <ul className='md:flex gap-4 m-2'>
+                {
+                    routes.map((route, idx) => <Link key={idx} route={route}></Link>)
+                }
+            </ul>
         </div>
     );
 };
